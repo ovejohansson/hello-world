4cd08b9e7e637e9512a1501c50d193cc8c414535
Ove Johansson <ove.johansson@stepout.se>
2016-03-03 17:57:00 +0100

    Add customesr table

---
+++ Tables/dbo.customers.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[customers]
+(
+[title] [varchar] (10) COLLATE Finnish_Swedish_CI_AS NULL,
+[name] [nchar] (50) COLLATE Finnish_Swedish_CI_AS NULL,
+[city] [varchar] (25) COLLATE Finnish_Swedish_CI_AS NULL
+) ON [PRIMARY]
+GO
