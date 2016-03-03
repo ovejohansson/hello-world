3ec09fc35775d66c70713132f2944dac3f156bb8
Ove Johansson <ove.johansson@stepout.se>
2016-03-03 18:22:43 +0100

    Ok

--- Tables/dbo.customers.sql
+++ Tables/dbo.customers.sql
@@ -1,9 +1,8 @@
 CREATE TABLE [dbo].[customers]
 (
 [title] [varchar] (10) COLLATE Finnish_Swedish_CI_AS NULL,
-[name] [nchar] (50) COLLATE Finnish_Swedish_CI_AS NULL,
 [city] [varchar] (25) COLLATE Finnish_Swedish_CI_AS NULL,
-[first_name] [varchar] (25) COLLATE Finnish_Swedish_CI_AS NULL,
-[last_name] [varchar] (25) COLLATE Finnish_Swedish_CI_AS NULL
+[first_name] [varchar] (25) COLLATE Finnish_Swedish_CI_AS NOT NULL,
+[last_name] [varchar] (25) COLLATE Finnish_Swedish_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
