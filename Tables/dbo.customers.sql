1b79e28e4edc793a9175f7ad1ed94fde64f733e6
Ove Johansson <ove.johansson@stepout.se>
2016-03-03 18:00:10 +0100

    first_name, last_name  added to customers

--- Tables/dbo.customers.sql
+++ Tables/dbo.customers.sql
@@ -2,6 +2,8 @@ CREATE TABLE [dbo].[customers]
 (
 [title] [varchar] (10) COLLATE Finnish_Swedish_CI_AS NULL,
 [name] [nchar] (50) COLLATE Finnish_Swedish_CI_AS NULL,
-[city] [varchar] (25) COLLATE Finnish_Swedish_CI_AS NULL
+[city] [varchar] (25) COLLATE Finnish_Swedish_CI_AS NULL,
+[first_name] [varchar] (25) COLLATE Finnish_Swedish_CI_AS NULL,
+[last_name] [varchar] (25) COLLATE Finnish_Swedish_CI_AS NULL
 ) ON [PRIMARY]
 GO
